32a8ca3b0d6f6fbc4b8195c586cacbea781b7a9e
MarchBeta2087 <pixel10m28@outlook.com>
2026-01-31 00:08:44 +0800

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// 👇 1. 引入 Tailwind v4 插件
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    // 👇 2. 把它加在这里
-    tailwindcss(),
-  ],
+  plugins: [vue(), tailwindcss()],
+  base: '/', // 个人主站必须是 '/'，如果是项目站则是 '/项目名/'
 })
